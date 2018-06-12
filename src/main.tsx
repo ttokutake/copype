76529b0cb989227c0a5aa5adfdc640828e39468b
ttokutake <tadatoshi.tokutake@gmail.com>
2018-06-12 18:08:30 +0900

    Add dummy "Copy" button

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,24 @@ import * as ReactDOM from "react-dom";
 import styled from "styled-components";
 
 const TextArea = styled.textarea`
-  height: 100%;
+  height: 90%;
   width: 100%;
 `;
 
+const Button = styled.button`
+`;
+
 class App extends React.Component {
+  copy() {
+    console.log("hello");
+  }
+
   render() {
     return (
-      <TextArea></TextArea>
+      <div>
+        <Button name="copy" onClick={this.copy}>Copy</Button>
+        <TextArea name="text"></TextArea>
+      </div>
     );
   }
 }
