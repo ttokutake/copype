0e5b79e845741fc1161476ffa0f8e4e38b555a5e
ttokutake <tadatoshi.tokutake@gmail.com>
2018-06-14 14:01:02 +0900

    Use "production" mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'prod' ? 'production' : 'development',
   devtool: "inline-source-map",
   entry: './src/main.tsx',
   output: {
